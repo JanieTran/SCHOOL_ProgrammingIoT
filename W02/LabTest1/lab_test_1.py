cf591055ec0ad8197ae2eb5535cc3af951e81aaa
Janie <emerald1901@gmail.com>
2019-06-27 11:32:45 +0700

    Shorten code for lab_test_1.py

--- W02/LabTest1/lab_test_1.py
+++ W02/LabTest1/lab_test_1.py
@@ -4,21 +4,16 @@ import pandas as pd
 
 
 def run():
-    url = 'https://api.myjson.com/bins/v94nb'
-
-    response = requests.get(url=url)
+    response = requests.get(url='https://api.myjson.com/bins/v94nb')
     json_data = json.loads(response.text)
 
     staff_df = pd.DataFrame(columns=['First Name', 'Last Name', 'Age'])
 
     for staff in json_data['staff']:
-        first_name = staff['FirstName']
-        last_name = staff['LastName']
-        age = staff['Age']
         staff_df = staff_df.append({
-            'First Name': first_name,
-            'Last Name': last_name,
-            'Age': age
+            'First Name': staff['FirstName'],
+            'Last Name': staff['LastName'],
+            'Age': staff['Age']
         }, ignore_index=True)
 
     staff_df.to_csv('staff.csv', index=False)
