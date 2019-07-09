a8fa596b92b311235efa630c3c764f69b7263e4f
Janie <emerald1901@gmail.com>
2019-07-09 09:55:58 +0700

    Lecture 6 - Unit test

--- W02/Lecture6/Example03/test_unittest.py
+++ W02/Lecture6/Example03/test_unittest.py
@@ -1,5 +1,6 @@
-import inc_dec    # The code to test
-import unittest   # The test framework
+import inc_dec  # The code to test
+import unittest  # The test framework
+
 
 class Test_TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
@@ -7,6 +8,7 @@ class Test_TestIncrementDecrement(unittest.TestCase):
 
     def test_decrement(self):
         self.assertEqual(inc_dec.decrement(3), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
